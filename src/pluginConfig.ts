4ab9671f82cf082829b7225a45d52ed2abb5c091
Yann Kerherve <ya.kerherve@gmail.com>
2026-02-24 15:37:24 +0100

    Update pluginConfig.ts

--- src/pluginConfig.ts
+++ src/pluginConfig.ts
@@ -2,7 +2,7 @@ import type { ExternalPluginConfig } from '@windy/interfaces';
 
 const config: ExternalPluginConfig = {
     name: 'windy-plugin-shipposition',
-    version: '3.0.0',
+    version: '3.0.1',
     icon: '🛰️',
     title: 'Ship Position',
     description: 'Ship Position',
