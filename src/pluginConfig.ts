8e41b26f4a122e3fe8c86039695996d35d87c449
Yann Kerherve <ya.kerherve@gmail.com>
2026-03-24 20:39:18 +0100

    Update pluginConfig.ts

--- src/pluginConfig.ts
+++ src/pluginConfig.ts
@@ -1,13 +1,13 @@
 import type { ExternalPluginConfig } from '@windy/interfaces';
 
 const config: ExternalPluginConfig = {
-    name: 'windy-plugin-shipposition',
-    version: '3.0.2',
-    icon: '🛰️',
-    title: 'Ship Position',
-    description: 'Ship Position',
+    name: 'windy-plugin-route',
+    version: '0.0.1',
+    icon: '🗺️',
+    title: 'Route',
+    description: 'Export your CSV from tactics and display it on windy',
     author: 'Yann Kerherve (ENSM)',
-    repository: 'https://github.com/YannKerherve/Ship-position',
+    repository: 'https://github.com/YannKerherve/route',
     desktopUI: 'embedded',
     mobileUI: 'small',
     routerPath: '/my-plugin',
