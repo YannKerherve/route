4acf870d984f04ee3aadc48cff9d7ee62ab29418
Yann Kerherve <ya.kerherve@gmail.com>
2026-03-30 20:24:37 +0200

    Update pluginConfig.ts

--- src/pluginConfig.ts
+++ src/pluginConfig.ts
@@ -2,10 +2,10 @@ import type { ExternalPluginConfig } from '@windy/interfaces';
 
 const config: ExternalPluginConfig = {
     name: 'windy-plugin-route',
-    version: '0.0.1',
+    version: '0.0.2',
     icon: '🗺️',
     title: 'Boat Route Tracker',
-    description: 'Export your CSV from tactics and display it on windy',
+    description: 'Export your CSV files from Tactics and Simsail, then display them on Windy. If you require a different CSV type, please contact me with an example or a licence for the relevant software.',
     author: 'Yann Kerherve (ENSM)',
     repository: 'https://github.com/YannKerherve/route',
     desktopUI: 'embedded',
