9607f34581616d0a69e0e629f5b807f0b0490bf6
Yann Kerherve <ya.kerherve@gmail.com>
2026-05-09 11:27:27 +0200

    Update pluginConfig.ts
    et oui on passe au .4 car les barbules étaient fausses de 180°

--- src/pluginConfig.ts
+++ src/pluginConfig.ts
@@ -2,7 +2,7 @@ import type { ExternalPluginConfig } from '@windy/interfaces';
 
 const config: ExternalPluginConfig = {
     name: 'windy-plugin-route',
-    version: '0.0.3',
+    version: '0.0.4',
     icon: '🗺️',
     title: 'Boat Route Tracker',
     description: 'Export your CSV files from Tactics, Simsail or Adrena, then display them on Windy. If you require a different CSV type, please contact me with an example or a licence for the relevant software.',
